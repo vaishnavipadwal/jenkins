54019e79ca11f19faa211d6dad3c7ad15bd9b03f
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-07-05 21:05:42 +0530

    Second  github file of python

---
+++ ifelse.py.py
@@ -0,0 +1,10 @@
+# Program to check if the number is positive, negative or zero
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive.")
+elif num == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
