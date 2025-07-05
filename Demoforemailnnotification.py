bd29569e81e20a1e6b432dbe84fd3e1cdc300c90
vaishnavipadwal <vaishupadwal12@gmail.com>
2025-07-05 21:30:33 +0530

    Demo for Email notification if code have error

---
+++ Demoforemailnnotification.py
@@ -0,0 +1,8 @@
+    # Write a program to check if a person is eligible to drive. The minimum voting age is 18.
+    a=int(input("enter your age: "))
+    print ("your age is: ",a)
+    if(a>=18):
+    print("you can drive")
+    else:
+    print("you cannot drive")
+
